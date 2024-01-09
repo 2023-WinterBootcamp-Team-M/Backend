7be3a6430cf3bdc6fa2bd0a33f0028d5d0413369
lsh1215 <vitash1215@gmail.com>
2024-01-09 23:20:34 +0900

    feat: 폴더, 북마크 생성 및 조회 완성 (중간저장) fix : url endpoint 수정

--- bookmark/serializer.py
+++ bookmark/serializer.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         # 어쨌든 필드의 값은 나중에 UserSerializer로 data를 간단하게 다룰 때 필요
         # 구글 소셜 로그인을 한다고 생각했을 때, 우리 DB로 그 내용을 옮긴다면
         # 이 3가지 필드 외에는 쓸 일이 없다고 생각 (생수삭은 제외해도 될듯)
-        fields = ('id', 'name', 'email')
+        fields = ['id', 'name', 'email']
 
 class FolderSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,14 +22,20 @@ class BookmarkSerializer(serializers.ModelSerializer):
         # 음 우선 folder_id가 필요할까? 나중에
         fields = '__all__'
 
+class FolderCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookmarkFolder
+        fields = ['id','user_id', 'name']
 
+class BookmarkCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookmark
+        fields = ['id', 'folder_id','name', 'url', 'icon']
 
-
-# class post_bookmarkSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Bookmark
-#         fields = '__all__'
-#
+class put_delete_BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookmark
+        fields = ['folder_id','name', 'url', 'icon']
 # class delete_bookmarkSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Bookmark
--- bookmark/urls.py
+++ bookmark/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 
 from .views import *
 
-app_name = ''
 urlpatterns = [
-    path('folders', create_folder, name=''),
-    path('folders/{user_id}', get_folders, name=''),
-    path('bookmarks/folder_id', get_bookmarks_in_folder, name=''),
-    path('bookmarks', create_bookmark, name=''),
-    path('folders/{folder_id}/bookmarks/{bookmarks_id}', update_bookmark, name=''),
-    path('folders/{folder_id}/bookmarks/{bookmarks_id}', delete_bookmark, name=''),
-    # path('bookmarks', , name=''),
+    path('folders', create_folder, name='create_folder'),
+    path('folders/<int:user_id>', get_folders, name='get_folders'),
+    path('bookmarks/<int:folder_id>', get_bookmarks_in_folder, name='get_bookmarks_in_folder'),
+    path('bookmarks', create_bookmark, name='create_bookmark'),
+    path('folders/<int:folder_id>/bookmarks/<int:bookmarks_id>', update_bookmark, name='update_bookmark'),
+    path('bookmarks/<int:folder_id>/<int:bookmarks_id>', delete_bookmark, name='delete_bookmark'),
+    path('Users', create_User, name='create_user'),
     # path('bookmarks', create_bookmark, name=''),
 ]
