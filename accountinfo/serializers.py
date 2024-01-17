0fdf072d418f972b4657a4b2ee43cb55330ddc8e
JuChan_L <20090839@vision.hoseo.edu>
2024-01-17 16:42:29 +0900

    feat option 기능 + profile read 기능

--- accountinfo/serializers.py
+++ accountinfo/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from accountinfo.models import accountinfo
+from accountinfo.models import accountinfo, accountoptions
+
 
 class UserDeleteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +21,24 @@ class UserSignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = accountinfo
         fields = ['email', 'password','user_name']
+
+class OptionCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = accountoptions
+        #fields = '__all__'
+        fields = ['accountid', 'summarizeoption', 'startupoption', 'themeoption', 'bookmarkalertoption']
+
+class OptionEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = accountoptions
+        fields = ['summarizeoption', 'startupoption', 'themeoption', 'bookmarkalertoption', 'updated_at']
+
+class OptionEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = accountoptions
+        fields = ['summarizeoption', 'startupoption', 'themeoption', 'bookmarkalertoption']
+
+class OptionIdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = accountoptions
+        fields = ['accountid']
