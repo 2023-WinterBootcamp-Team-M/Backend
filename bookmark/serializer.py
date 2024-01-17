db188a6e8a86f29fd26dbdf7481fe52d58709818
sanghun <vitash1215@gmail.com>
2024-01-17 17:15:49 +0900

    fix : 진짜 진짜 오류 수정

--- bookmark/serializer.py
+++ bookmark/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Bookmark, BookmarkFolder, User
+from .models import Bookmark, BookmarkFolder
 
 
 class FolderSerializer(serializers.ModelSerializer):
