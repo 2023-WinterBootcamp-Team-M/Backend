a236b01b80aac4f69c8c2df32f64652ccae5f9cb
Kiminni <imk0980@gmail.com>
2024-01-22 17:56:48 +0900

    fix: settings.py allowed_hosts 추가

--- django_back/settings.py
+++ django_back/settings.py
@@ -148,6 +148,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['127.0.0.1',
                  '127.0.0.1:8000',
                  'localhost:8000',
+                 'host.docker.internal',
                  'host.docker.internal:8000',
                  'node-exporter:9100',
                  'prometheus:9090',
